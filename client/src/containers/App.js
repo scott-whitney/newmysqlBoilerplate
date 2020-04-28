4e3ad2c8c6c11046b79752a590ca8dd4d2870282
Minroe <scott.whitney334@gmail.com>
2020-04-28 15:10:39 -0700

    another step

---
+++ client/src/containers/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Counter from './Counter';
+import TodoForm from './TodoForm';
+
+const App = () => {
+  return (
+    <>
+      <Counter/>
+      <TodoForm/>
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/containers/TodoForm/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import RenderTodoList from '../../components/RenderTodoList';
+
+class TodoForm extends Component {
+  state = {
+    todos: [],
+    todoInput: ''
+  }
+  async componentDidMount() {
+    console.log("Inside componentDidMount");
+    try {
+      const { data } = await axios.get('/api/todos');
+      this.setState({ todos: data });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  handleInputChange = event => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  }
+  handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post('/api/todos', {text: this.state.todoInput });
+      const todos = [...this.state.todos, data];
+      this.setState({ todos, todoInput: '' });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    console.log("I rendered inside of Form");
+    return (
+      <div>
+        <RenderTodoList items={this.state.todos}/>
+        <form>
+          <input
+            name="todoInput"
+            value={this.state.todoInput}
+            onChange={this.handleInputChange}
+          />
+          <button onClick={(e) => this.handleSubmit(e)}>Add todo</button>
+        </form>
+      </div>
+    );
+  }
+}
+export default TodoForm;
